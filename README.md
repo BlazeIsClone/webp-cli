660da5ab7ace88563b8e616e114dc1420e9ecff9
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-10-29 18:29:52 +0530

    init: repository

---
+++ README.md
@@ -0,0 +1,11 @@
+# WebP CLI
+
+A node library for converting .png and .jpg to .webp using [webp-converter](https://github.com/scionoftech/webp-converter).
+
+## Commands
+
+```bash
+npm start
+```
+
+Running this will create a .webp copy on the same path as any .png or .jpg file (node_modules is excluded by default).
---
+++ bin/webp-cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const { cosmiconfig } = require('cosmiconfig');
+const chalk = require('chalk');
+
+const converter = require('../lib');
+
+const { info } = console;
+
+(() => {
+	info(chalk.gray('> Starting WebP CLI'));
+	info(chalk.blue('> Scanning for images'));
+
+	const explorer = cosmiconfig('webp-converter');
+
+	explorer
+		.search()
+		.then(result => {
+			converter(result);
+		})
+		.catch(error => {
+			info(chalk.red(error));
+		});
+})();
